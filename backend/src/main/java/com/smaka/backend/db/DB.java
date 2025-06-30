a5b8041df59e146b86130b3fea42fdcdd84e96ea
Павле Шмакић <sp190347d@student.etf.bg.ac.rs>
2025-06-30 12:28:59 +0200

    DB connect

---
+++ backend/src/main/java/com/smaka/backend/db/DB.java
@@ -0,0 +1,22 @@
+package com.smaka.backend.db;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DB {
+
+    @Bean
+    public static DataSource source(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+}
