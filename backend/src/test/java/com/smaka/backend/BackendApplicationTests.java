6e525c41c2b16cddb4ffcf6eef7456fc0b45b994
Павле Шмакић <sp190347d@student.etf.bg.ac.rs>
2025-06-30 10:19:09 +0200

    Fix: remove frontend submodule, add as normal folder

---
+++ backend/src/test/java/com/smaka/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smaka.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
